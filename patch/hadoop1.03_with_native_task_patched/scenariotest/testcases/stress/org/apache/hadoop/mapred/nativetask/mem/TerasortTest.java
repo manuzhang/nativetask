446deb91b47abb849fa938c8779a21d4f6b5b03a
zoken <zhuzhongliang1990@gmail.com>
2013-07-04 12:40:20 +0800

    Add stress testcases: TerasortTest,WordCountTest

---
+++ patch/hadoop1.03_with_native_task_patched/scenariotest/testcases/stress/org/apache/hadoop/mapred/nativetask/mem/TerasortTest.java
@@ -0,0 +1,32 @@
+package org.apache.hadoop.mapred.nativetask.mem;
+
+import org.apache.hadoop.mapred.nativetask.tools.TeraSort;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TerasortTest {
+	private String terainput = "./testfile/terainput";
+	private String teraoutput = "./testfile/output/teraNativeOut";
+	@Before
+	public void startUp(){
+		
+	}
+	@After
+	public void tearDown(){
+		
+	}
+	@Test
+	public void testTeraSort(){
+		String[] args = {terainput,teraoutput};
+		int exitCode = -1;
+		try {
+			exitCode = new TeraSort().run(args);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			System.err.println("testTeraSort finished with exitCode = "+exitCode);
+		}
+	}
+}
